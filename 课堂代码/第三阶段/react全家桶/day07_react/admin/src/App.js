33b47c59581fa333c6f2b5f909f6e6d5783dc409
lupeng <749003013@qq.com>
2021-10-28 21:48:33 +0800

    a

---
+++ 课堂代码/第三阶段/react全家桶/day07_react/admin/src/App.js
@@ -0,0 +1,20 @@
+import './App.less';
+
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <br />
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
